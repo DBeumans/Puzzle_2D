1f18455b2dc7ffa52dfc8cdd4ed79e89da76ce96
Danivdwerf <danivdwerf.com>
2017-02-20 14:56:58 +0100

    added background

--- Assets/Scripts/TerminalInput/FetchTerminalInput.cs
+++ Assets/Scripts/TerminalInput/FetchTerminalInput.cs
@@ -14,6 +14,7 @@ public class FetchTerminalInput : MonoBehaviour
 	{
 		checkInput = GetComponent<CheckTerminalInput> ();
 		output = GetComponent<ShowOutput> ();
+		index = 0;
 		resetInput ();
 	}
 
@@ -47,6 +48,10 @@ public class FetchTerminalInput : MonoBehaviour
 		{
 			index = 0;
 		}
-		inputField.text = checkInput.getPreviousCommands [index];
+		if (checkInput.getPreviousCommands.Count > 0)
+		{
+			inputField.text = checkInput.getPreviousCommands [index];
+		}
+		return;
 	}
 }
