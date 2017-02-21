c7b3c044ee5ea762abea362db2e9c28aed0852c9
Danivdwerf <danivdwerf.com>
2017-02-21 10:07:25 +0100

    open terminal with button

---
+++ Assets/Scripts/TerminalUI/ComputerUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerUI : MonoBehaviour 
+{
+	[SerializeField]private GameObject terminal;
+	[SerializeField]private Button terminalIcon;
+
+	private void Start()
+	{
+		terminalIcon.onClick.AddListener (delegate(){enableTerminal(true);});	
+	}
+
+	private void enableTerminal(bool value)
+	{
+		terminal.SetActive (value);
+		this.enabled = false;
+	}
+}
