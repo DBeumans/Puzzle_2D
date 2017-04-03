66cddfd1085c028da64e7cf577d49b86c2ca5415
DBeumans <buncho.gamingnetwork@gmail.com>
2017-04-03 20:44:48 +0200

    player movement fix

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private InputBehaviour inputBehaviour;
 
+    [SerializeField]private float oldMovementSpeed;
     [SerializeField]private float movementSpeed;
     [SerializeField]private bool isGrounded;
     [SerializeField]private float jumpPower;
@@ -28,58 +29,64 @@ public class PlayerMovement : MonoBehaviour {
         inputBehaviour = FindObjectOfType<InputBehaviour>();
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myTriggerCollision = GetComponent<PlayerTriggerCollision>();
+        oldMovementSpeed = movementSpeed;
 	}
 
-    private void FixedUpdate ()
+    private void Update ()
     {
         CheckRaycast();
 
-        if (inputBehaviour.GetJumpKey)
-        {
-            if(jumpCounter < maxJumpCount-1)
-            {
-                jumpCounter++;
-                myRigidbody2D.AddForce(Vector2.up * (jumpPower *1000)* Time.deltaTime);
-            }
-        }
+        
         if(isGrounded)
         {
             jumpCounter--;
         }
         if (jumpCounter < 0)
             jumpCounter = 0;
-        
+
+        if (inputBehaviour.GetJumpKey)
+        {
+            print("jump");
+            if (jumpCounter < maxJumpCount - 1)
+            {
+                jumpCounter++;
+                //myRigidbody2D.AddForce(Vector2.up * (jumpPower *1000)* Time.deltaTime);
+                myRigidbody2D.AddForce(new Vector2(0, jumpPower));
+            }
+        }
+
         //myRigidbody2D.velocity = new Vector2(inputBehaviour.GetMoveInput.x * movementSpeed, myRigidbody2D.velocity.y);
 
         if (inputBehaviour.GetKeyA ) //left
         {
-            if(myDirection != -1 )
-            {
-                myRigidbody2D.velocity = new Vector2(-movementSpeed, myRigidbody2D.velocity.y);
-                transform.eulerAngles = new Vector2(0, 0);
-                myDirection = -1;
-            }     
+            myDirection = -1;
+            myRigidbody2D.velocity = new Vector2(-movementSpeed, myRigidbody2D.velocity.y);
+            transform.eulerAngles = new Vector2(0, 0);
         }
 
         else if (inputBehaviour.GetKeyD ) // right
         {
-            if(myDirection != 1 )
-            {
-                if (myWall)
-                {
-                    myRigidbody2D.AddForce(Vector2.right * 10);
-                }
-                myRigidbody2D.velocity = new Vector2(movementSpeed, myRigidbody2D.velocity.y);
-                transform.eulerAngles = new Vector2(0, 180);
-                myDirection = 1;
-            }   
+            myDirection = 1;
+            
+            myRigidbody2D.velocity = new Vector2(movementSpeed, myRigidbody2D.velocity.y);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        
+
+        else if (myWall && isGrounded== false && Mathf.Abs(myDirection) > .1f )
+        {
+            movementSpeed = 0;
+            //myRigidbody2D.velocity = new Vector2(0, myRigidbody2D.velocity.y);
         }
 
         else
         {
             myRigidbody2D.velocity = new Vector2(0, myRigidbody2D.velocity.y);
             myDirection = 0;
+            movementSpeed = oldMovementSpeed;
         }
+
+        
     }
 
     private void CheckRaycast()
